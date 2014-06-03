f8d33479fe8d863797bdbb78db3c88a1c2e2e976
root <root@grade.net>
2014-06-03 00:27:00 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,79 @@
+formsync.js 1.0
+------------------------------------
+Formsync is an ultra lightweight jQuery add on that allows a developer to asynchronously submit forms without understanding or writing javascript.
+
+Installation
+-----------------------------
+
+
+Building the Form, Frontend
+-----------------------------
+If you've written a HTML form in the past then there isn't anything new. Simply build your form as usual and add the "formsync" class to the <form> tag. For messages add two div layers inside the <form> one with a class of "error" the other "success". These should be styled by you. 
+
+<form class="formsync" method="GET" action="submit.php">
+<div class="error" style="display:none;"></div>
+<div class="success" style="display:none;"></div>
+
+<input type="text" name="hello">
+<input type="submit" value="Submit Form">
+</form>
+
+That's all you're ready to go!
+
+
+
+Sending Responses, Backend
+-----------------------------
+The action URL should always send back a JSON response. For simplicity formsync.js expects one variable called "error" to be returned. In the case of a success "error" should be 0, NULL, or false.
+
+Most frameworks have built in support for JSON, here is a sample case for "submit.php".
+<?php
+
+echo json_encode(array("error"=>"Invalid Email Address"));
+
+?>
+
+formsync.js will automatically inject this error message into <div class="error"></div>.
+
+
+Form Attributes
+-----------------------------
+formsync_method
+Overrides the method (if any) specified in <form method="POST"> 
+<input type="hidden" name="formsync_method" value="http://grade.net">
+
+
+formsync_action
+Overrides the action (if any) specified in <form action="submit.php">
+<input type="hidden" name="formsync_action" value="http://grade.net">
+
+
+
+Success Actions
+-----------------------------
+formsync_redirect
+Redirect user to http://grade.net if server returns no error on form submission.
+<input type="hidden" name="formsync_redirect" value="http://grade.net">
+
+formsync_message
+Inject message into <div class="success"></div> if server returns no error on form submission.
+<input type="hidden" name="formsync_message" value="The form was submitted successfully.">
+
+formsync_alert
+Uses the javascript alert function to display a message
+<input type="hidden" name="formsync_alert" value="Thanks for your feedback.">
+
+formsync_callback
+The name of a user written function to call after success
+<input type="hidden" name="formsync_callback" value="myFunctionName">
+
+formsync_clear
+Clear all input fields of data on success
+<input type="hidden" name="formsync_message" value="true">
+
+
+Notices
+----------------------------
+"formsync" is a restricted keyword.
+Do not use the text "formsync" to define variables or inputs outside the ones listed here.
+Doing so may unintentionally cause data loss.
---
+++ formsync.js
@@ -0,0 +1,221 @@
+$(document).ready(function() {
+
+	/**
+	- Script: formsync.js
+	- Description: Allows the asynchronous submission of forms. Without
+	  requiring the end developer to understand or use javascript.
+	- Author: Zach Alam
+	- Website: www.zachalam.com
+	- Last Update: 2 June 2014
+	*/
+
+	////////////////////////////////////////////////////////
+
+	// enable/disable debug mode.
+	// this shows specific errors when server does not accept data
+	var DEBUG_MODE_ENABLED = false;
+	
+	// language variables
+	// shown when the submit button is pressed
+	var LANG_PLEASE_WAIT = "Please wait...";
+	// generic error, shown when debug mode is false, and server error occurs.
+	var LANG_UNABLE_TO_COMM = "Unable to communicate with server.";
+	
+	// error selector, used to inject server responses 
+	// <div class="error"></div>
+	var ERROR_SELECTOR = ".error";
+	// <div class="success"></div>	
+	var SUCCESS_SELECTOR = ".success";
+	
+	////////////////////////////////////////////////////////
+	
+	$(".formsync").submit(function(event) {
+	
+		// Generally, browsers navigate to the "action"
+		// specified in the attribute of a HTML <form> tag.
+		// by specifying the .preventDefault() method
+		// we can prevent this action from happening.
+		event.preventDefault();
+		
+		// hide open error or success div if shown
+		$(ERROR_SELECTOR, this).slideUp();
+		$(SUCCESS_SELECTOR, this).slideUp();
+		
+		// assign global variable to this formsync
+		window.formsync = this;
+		
+		// disallow user from making changes to any part of the 
+		// form when they want to submit it.
+		$(':input',this).prop("disabled",true);
+		
+		// save the current text value of the submit button
+		window.submit_button = $('input[type="submit"]',this).attr("value");
+		
+		// set the default text of the submit button to please wait.
+		$('input[type="submit"]',this).attr("value",LANG_PLEASE_WAIT);
+		
+		// -----------------------------------------------------------
+		
+		// obtain whether this is a GET or POST
+		var method_override = formsync_get_attribute("method");
+		if(method_override) method = method_override;
+		else method = $(this).attr("method");
+		// if no method is set, use GET
+		if(!method) method = "GET";
+		
+		// obtain the action of this form, ie: submit.php
+		var action_override = formsync_get_attribute("action");
+		if(action_override) action = action_override;
+		else action = $(this).attr("action");
+		
+		// -----------------------------------------------------------
+
+		// serialize formdata
+		var form_data = $(this).serialize();
+
+		// strip all inputs with formsync_ in front of them
+		
+
+
+		// set timeout of 400ms, for better visual appearance.
+		// this prevents the server from responding too quickly, causing 
+		// the input fields from flashing.
+		setTimeout(function() {	
+
+			// send request with data to server
+			$.ajax({
+				type: method,
+				url: action,
+				data: form_data,
+				success: formsync_success,
+				error: formsync_error,
+				complete: formsync_complete,
+				dataType: 'json'
+			});
+		
+		}, 400);
+		
+	});
+	
+	////////////////////////////////////////////////////////
+	
+	function formsync_get_attribute(name)
+	{
+		// retrieves hidden input field value from "thisform"
+		// <input type="hidden" name="formsync_method" value="POST">
+		// ie: formsync_method
+		
+		var attribute = $('input[name="formsync_'+name+'"]',window.formsync).attr("value");
+		return attribute;	
+	}
+	
+	function formsync_set_error(error_text)
+	{
+		// sets <div class="error"> to the error text
+		// then displays it to the end user.
+		
+		$(ERROR_SELECTOR,window.formsync).html(error_text);
+		$(ERROR_SELECTOR,window.formsync).slideDown();	
+	}
+	
+	////////////////////////////////////////////////////////
+	
+	function formsync_success(data)
+	{
+		// the server did not encouter a problem handling this request
+		if(data.error)
+		{
+			// server did not accept data
+			formsync_set_error(data.error);
+			
+		}
+		else
+		{
+			// server accepted data, read formsync inputs
+			// to find out what happens next
+			if(formsync_get_attribute("redirect"))
+			{
+				// redirect browser
+				window.location.href = formsync_get_attribute("redirect");
+			}
+
+			// ----------
+
+			else if(formsync_get_attribute("message"))
+			{
+				// sets a message to the "success" div
+				$(SUCCESS_SELECTOR,window.formsync).html(formsync_get_attribute("message"));
+				$(SUCCESS_SELECTOR,window.formsync).slideDown();					
+
+			}
+
+			// ----------
+
+			else if(formsync_get_attribute("alert"))
+			{
+				// sets a message using the alert() function.
+				alert(formsync_get_attribute("alert"));
+
+			}
+
+			// ----------
+		
+			else if(formsync_get_attribute("callback"))
+			{
+				// runs a user defined function
+				var call_callback = window[formsync_get_attribute("callback")];
+				call_callback();
+
+			}
+
+			// ----------
+
+			if(formsync_get_attribute("clear"))
+			{
+				// reset form
+				$(window.formsync)[0].reset();
+
+			}						
+			
+		}
+	}
+	
+	function formsync_error(data)
+	{
+		// the server encountered a problem handling this request
+		
+		if(DEBUG_MODE_ENABLED)
+		{
+			// an error occurred talking to the server.
+			// post an alert to the user
+			alert(data.status + ", " + data.statusText);
+			
+			// inject response error into error div
+			formsync_set_error(data.responseText);
+		}
+		else
+		{
+			// show generic error
+			formsync_set_error(LANG_UNABLE_TO_COMM);
+		}
+	}
+	
+	function formsync_complete()
+	{
+		// function to complete after ajax request
+		// regardless of whether the form was successfull submitted
+
+
+	
+		// change submit button text back to original
+		$("input[type='submit']",window.formsync).attr("value",window.submit_button);
+		
+		// enable all inputs
+		$(':input',window.formsync).prop("disabled",false);
+
+
+				
+	}
+	
+	
+});
